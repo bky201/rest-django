d2ec567e93a3410008e43d59b23e54348a05b6c4
bek <bekrye@gmail.com>
2023-09-03 18:09:38 +0200

    API Project Setup with Django and Cloudinary

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ["IP"] = "0.0.0.0"
+os.environ["PORT"] = "8000"
+os.environ["SECRET_KEY"] = ""
+os.environ["DATABASE_URL"] = ""
+os.environ["CLOUDINARY_URL"] = "cloudinary://115381342371743:gnHVSDgRpmNV0bL91lvhhbr3Z-k@dliocvbsf"
