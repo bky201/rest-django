ff124a8ad5f5b12c2c8ce50184f6cc3891ed0919
bek <bekrye@gmail.com>
2023-09-03 20:21:49 +0200

    Remove file

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ["IP"] = "0.0.0.0"
-os.environ["PORT"] = "8000"
-os.environ["SECRET_KEY"] = ""
-os.environ["DATABASE_URL"] = ""
-os.environ["CLOUDINARY_URL"] = "cloudinary://758346658131797:qCCqDJb9fs2a96O2FdBJahl1Isg@asujexujexk"
