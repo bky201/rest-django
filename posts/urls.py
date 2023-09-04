767371ac823353e0a5d9420ecb543f13909fe875
bek <bekrye@gmail.com>
2023-09-04 18:43:12 +0200

    Add PostDetail view to retrieve, edit and delete a post

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from posts.views import PostList
+from posts.views import PostList, PostDetail
 
 urlpatterns = [
     path('posts/', PostList.as_view()),
+    path('posts/<int:pk>/', PostDetail.as_view()),
 ]
