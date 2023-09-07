6a4f6a630c11fdf338c55b6ab07373d1f07db1cf
bek <bekrye@gmail.com>
2023-09-07 22:32:15 +0200

    Deployment commit

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,7 +63,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['djangorest-api-0a8f6a34c732.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 
